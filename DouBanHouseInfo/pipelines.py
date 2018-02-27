d2e3584ede9f1d1f7b0859fb3a2299fbc7720195
jichunMa <jichunma@yangqianguan.com>
2018-02-27 14:08:24 +0800

    稳定可用版本v1.0

--- DouBanHouseInfo/pipelines.py
+++ DouBanHouseInfo/pipelines.py
@@ -10,6 +10,14 @@ import xlwt
 
 class DoubanhouseinfoPipeline(object):
 
+    def validation_data(self, src_data):
+        # todo 自定义关键字(满足条件的关键字数据将入文件保存)
+        keyword_list = ['金台夕照', '团结湖', '呼家楼', '劲松', '潘家园', '大望路', '四惠']
+        for keyword in keyword_list:
+            if keyword in src_data:
+                return True
+        return False
+
     def open_spider(self, spider):
         self.index = 1
         self.book = xlwt.Workbook()
@@ -20,12 +28,16 @@ class DoubanhouseinfoPipeline(object):
         pass
 
     def close_spider(self, spider):
-        self.book.save('reuslt_house.xls')
+        # todo 文件输出路径
+        self.book.save('/Users/majichun/Desktop/reuslt_house.xls')
         pass
 
     def process_item(self, item, spider):
-        self.sheet.write(self.index, 0, item['title'])
-        self.sheet.write(self.index, 1, item['time'])
-        self.sheet.write(self.index, 2, item['link'])
-        self.index += 1
+        title = item['title']
+        # 清洗数据对合法数据进行输出保存
+        if self.validation_data(title):
+            self.sheet.write(self.index, 0, item['title'])
+            self.sheet.write(self.index, 1, item['time'])
+            self.sheet.write(self.index, 2, item['link'])
+            self.index += 1
         return item
--- DouBanHouseInfo/spiders/DouBanHouseInfospider.py
+++ DouBanHouseInfo/spiders/DouBanHouseInfospider.py
@@ -3,10 +3,31 @@ from scrapy.selector import Selector
 from DouBanHouseInfo.items import DoubanhouseinfoItem
 
 
-class douban_house_info(CrawlSpider):
+class DouBanHouseInfo(CrawlSpider):
     name = 'doubanhouseinfo'
     start_urls = ['https://www.douban.com/group/zhufang/discussion?start=1']
+
+    # todo 小组渠道名称
+    channel_name_list = ['zhufang', 'fangzi', 'sweethome', 'opking', 'beijingzufang', '26926']
+    # todo 爬取页数
+    crawl_page = 3
+    # 基本格式 url
+    base_url = "https://www.douban.com/group/{}/discussion?start={}"
+    # 排重使用
     title_dict = {}
+
+    # 构造小组url
+    def build_urls(self, channel_names):
+        urls = []
+        for name in channel_names:
+            for index in range(self.crawl_page):
+                url = self.base_url.format(name, index * 25)
+                urls.append(url)
+        return urls
+
+    def __init__(self):
+        self.logger.info('init')
+        self.start_urls = self.build_urls(self.channel_name_list)
 
     def parse(self, response):
         selector = Selector(response)
@@ -16,7 +37,6 @@ class douban_house_info(CrawlSpider):
             href_list = div.xpath('./td[1]/a/@href').extract()
             time_list = div.xpath('./td[4]/text()').extract()
             title = ''
-            href = ''
             get_time = ''
             if len(title_list) > 0:
                 title = title_list[0].replace('\n', "").replace(' ', '')
@@ -34,4 +54,4 @@ class douban_house_info(CrawlSpider):
                         item['time'] = get_time
                         yield item
                     else:
-                        print(title)
+                        pass
